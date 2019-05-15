51affe1a31a2075b4f6825c1f44c9d8d0cd52fab
Matt Smith <msmith@valorem.com>
2019-05-15 15:40:13 -0500

    Initial commit.

---
+++ CloneAppServiceConfigSetings.ps1
@@ -0,0 +1,18 @@
+﻿Login-AzureRmAccount
+# If you have multiple subscriptions, you may need to set your context.
+Set-AzureRmContext -Subscription 'SubscriptionName'
+
+$myResourceGroup = 'ResourceGroup'
+$mySite = 'OldAppService'
+$myResourceGroup2 = 'ResourceGroup'
+$mySite2 = 'NewappService'
+
+$props = (Invoke-AzureRmResourceAction -ResourceGroupName $myResourceGroup `
+        -ResourceType Microsoft.Web/sites/Config -Name $mySite/appsettings `
+        -Action list -ApiVersion 2015-08-01 -Force).Properties
+
+$hash = @{}
+$props | Get-Member -MemberType NoteProperty | % { $hash[$_.Name] = $props.($_.Name) }
+
+Set-AzureRMWebApp -ResourceGroupName $myResourceGroup2 `
+        -Name $mySite2 -AppSettings $hash
